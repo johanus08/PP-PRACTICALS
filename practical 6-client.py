a1aa8db5ca0ac79c72d8072296eaa40208f989f9
johanus08 <60962562+johanus08@users.noreply.github.com>
2020-03-13 17:24:33 +0530

    Update practical 6-client.py

--- practical 6-client.py
+++ practical 6-client.py
@@ -1,12 +1,16 @@
 import socket
-# create a socket object
+
+# Create a socket object
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-# get local machine name
+
+# Get local machine name
 host = socket.gethostname()
 print(host)
 port = 9999
-# connection to hostname on the port.
-s.connect((host, port))                               
+
+# Connection to hostname on the port.
+s.connect((host, port)) 
+
 # Receive no more than 1024 bytes
 msg = s.recv(1024)                                     
 s.close()
