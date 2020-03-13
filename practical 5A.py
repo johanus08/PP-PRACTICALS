a510f39816ed466f1770d5efb6c2a8c341479b03
johanus08 <60962562+johanus08@users.noreply.github.com>
2020-03-13 17:27:27 +0530

    Update practical 5A.py

--- practical 5A.py
+++ practical 5A.py
@@ -1,9 +1,18 @@
+# Python program to draw on a canvas using tkinter
+# Importing tkinter module
 from tkinter import*
+
+# Master window
 root=Tk()
+
+# Setting height width
 canvas_width=500
 canvas_height=500
+
+# Canvas created
 c=Canvas(root,width=canvas_width,height=canvas_height,bg="blue")
 c.pack()
+
 y=int(canvas_height/2)
 c.create_line(10,y,canvas_width,y,fill="green")
 coord=10,50,240,210
